43307176949cee6717c619ccde2d56ece1a7fdab
fjoollab <167626907+fjoollab@users.noreply.github.com>
2025-01-19 19:50:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+        question: "Wich is largest animal in the world",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Wich is the smallest continent in the world",
+        answers: [
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false},
+        ]
+    },
+    {
+        question: "Wich is largest desert in the world",
+        answers: [
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: false},
+            {text: "Antractica", correct: true},
+        ]
+    }
+];
